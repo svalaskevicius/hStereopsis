17a6bdd887da6a6de3d66a9c842fcbd1580597fb
Sarunas Valaskevicius <rakatan@gmail.com>
2012-09-05 15:52:59 +0100

    simplify code

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 module Main where
---import           CV.Bindings.Types
+
+import           Control.Monad
 import           CV.ColourUtils
-import           CV.DrawableInstances
-import           CV.Drawing
+import           CV.DrawableInstances()
 import           CV.Features
 import           CV.Filters
+import           CV.HighGUI
 import           CV.Image
 import           CV.ImageMathOp
 import           CV.ImageOp
 import           CV.Transforms
 import           System.Environment
-import           System.IO.Unsafe
 import           Utils.DrawingClass
---import           Utils.GeometryClass
-import           Control.Monad
 
 delit :: Image GrayScale D32 -> Image GrayScale D32
 delit i = i #- gaussian (41,41) i
@@ -25,21 +23,12 @@ main = do
    Just x <- liftM
         (fmap (stretchHistogram . gaussian (13,13) . delit))
         (getArgs >>= loadImage . head)
-   let y = scale Area (2.2,2) . rotate (pi/2) $ x
+   let y = rotate (pi/2) x
        lst  = getSURF defaultSURFParams (unsafeImageTo8Bit x) Nothing
        lsty = getSURF defaultSURFParams (unsafeImageTo8Bit y) Nothing
-   let result lst x = (x <## map (draw.fst) lst) :: Image GrayScale D32
-  -- [ellipseBoxOp 1 (C'CvBox2D c size d) 1 0
-  --                    | (C'CvSURFPoint c l s d h,_) <- lst
-  --                    , let size = C'CvSize2D32f (fromIntegral s) (fromIntegral $ s`div`2)
-  --                    ]
-   saveImage "surf_result.png" $ montage (1,2) 2 [x, result lst x] -- ,result lsty y]
+   let result _lst _x = (_x <## map (draw.fst) _lst) :: Image GrayScale D32
    mapM_ print (take 5 lst)
+   display $ scaleToSize Area True (1024, 750) $ montage (2,2) 3 [x, result lst x, result lsty y]
+   return ()
 
-padToSize :: (CreateImage (Image GrayScale D32)) =>  (Int,Int)
-                                                    -> Image GrayScale D32
-                                                    -> Image GrayScale D32
-padToSize size img = unsafePerformIO $ do
-    let r = empty size
-    blit r img (0,0)
-    return r
+

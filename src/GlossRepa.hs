1f492c5576de1d8665eb5ee2c52279c80a5e1c0b
Sarunas Valaskevicius <rakatan@gmail.com>
2012-09-07 23:55:32 +0100

    calculate sobel magnitude + thetas of found edges

--- src/GlossRepa.hs
+++ src/GlossRepa.hs
@@ -5,7 +5,8 @@ module GlossRepa
   , repaToPicture
   , readRepaImage
   , toGrayscale
-  , gradientX, gradientY
+  , floatToGrayscale
+  , sobel
   ) where
 
 import           Control.Monad
@@ -16,8 +17,8 @@ import           Data.Array.Repa.IO.DevIL        as RD
 import           Data.Array.Repa.Repr.ForeignPtr as R
 import           Data.Word
 import qualified Graphics.Gloss                  as G
-import Data.Array.Repa.Stencil          as R
-import Data.Array.Repa.Stencil.Dim2     as R
+import           Data.Array.Repa.Stencil         as R
+import           Data.Array.Repa.Stencil.Dim2    as R
 
 force
   :: ( RE.Load r1 sh e, RE.Target r2 e, Source r2 e)
@@ -47,7 +48,9 @@ toGrayscale (RD.RGBA arr) = RD.Grey
                 (\ (Z :. i :. j :. _) -> (Z :. i :. j))
                 (\f (Z :. i :. j) -> rgbToGrayScale (f (Z :. i :. j :. 3)) (f (Z :. i :. j :. 2)) (f (Z :. i :. j :. 1)))
         )
-        
+
+floatToGrayscale :: Array U DIM2 Float -> RD.Image 
+floatToGrayscale arr = RD.Grey (force $ R.map (truncate.(*256)) arr)
         
 toRgba :: RD.Image -> RD.Image
 toRgba (RD.Grey arr) = RD.RGBA (grayToRgba arr)
@@ -64,24 +67,28 @@ readRepaImage = RD.runIL . RD.readImage
 
 
 
-gradientX :: RD.Image -> RD.Image
-gradientX (RD.Grey img) = RD.Grey (force $ mapStencil2 (BoundConst 0) stencil img)
+gradientX :: Array F DIM2 Word8 -> Array PC5 DIM2 Float
+gradientX img = mapStencil2 (BoundConst 0) stencil $ R.map ((/ 256).fromIntegral) img
         where stencil = [stencil2| -1  0  1
                                    -2  0  2
                                    -1  0  1 |]
-gradientX _ = error "unspported image type for gradient calculation"
 
-gradientY :: RD.Image -> RD.Image
-gradientY (RD.Grey img) = RD.Grey (force $ mapStencil2 (BoundConst 0) stencil img)
+gradientY :: Array F DIM2 Word8 -> Array PC5 DIM2 Float
+gradientY img = mapStencil2 (BoundConst 0) stencil $ R.map ((/ 256).fromIntegral) img
         where stencil = [stencil2| 1  2  1
                                    0  0  0
                                   -1 -2 -1 |] 
-gradientY _ = error "unspported image type for gradient calculation"
 
 
-
-
-
+sobel :: RD.Image -> (Array U DIM2 Float, Array U DIM2 Float)
+sobel (RD.Grey img) = (force magnitudes, force thetas)
+        where
+        gx = gradientX img
+        gy = gradientY img
+        thetaFromDiff x y = if x == 0 then if y < 0 then -pi else if y > 0 then pi else 0 else atan (y / x)
+        magnitudes = R.zipWith (\x y -> sqrt (x * x + y * y)) gx gy
+        thetas = R.zipWith thetaFromDiff gx gy
+sobel _ = error "sobel operator only works on grayscale images"
 
 
 
--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,10 @@
 
 module Main where
 
+import           Data.Array.Repa.IO.Matrix
 import           GlossRepa
 import           Graphics.Gloss
 import           System.Environment
-
 
 main :: IO()
 main = do
@@ -17,8 +17,10 @@ main = do
 run :: FilePath -> IO ()
 run fileName = do
         img <- readRepaImage fileName
-        let (width, height, picture) = repaToPicture True ((gradientY . toGrayscale) img)
-
+        let greyImg = toGrayscale img
+        let (sbMag, thetas) = sobel greyImg
+            (width, height, picture) = repaToPicture True (floatToGrayscale sbMag)
+        writeMatrixToTextFile "test.dat" thetas
         putStrLn ("x: " Prelude.++ show width Prelude.++ ", Y: " ++ show height)
-        display (InWindow fileName (width, height) (10,  10)) black picture
+        display (InWindow fileName (width, height) (10,  10)) white picture
 

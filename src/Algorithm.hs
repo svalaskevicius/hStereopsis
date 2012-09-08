84b926a6337b1daa180ba0cc69db92dbe85611ab
Sarunas Valaskevicius <rakatan@gmail.com>
2012-09-08 19:27:27 +0100

    add gaussian kernel convolution function

--- src/Algorithm.hs
+++ src/Algorithm.hs
@@ -3,25 +3,24 @@
 module Algorithm
   (
         sobel
+        , gaussian
   ) where
 
 import           Data.Array.Repa                 as R
 import           Data.Array.Repa.Stencil         as R
 import           Data.Array.Repa.Stencil.Dim2    as R
 
-
 gradientX :: (Source a Float) => Array a DIM2 Float -> Array PC5 DIM2 Float
-gradientX = mapStencil2 (BoundConst 0) stencil
+gradientX = mapStencil2 BoundClamp stencil
         where stencil = [stencil2| -1  0  1
                                    -2  0  2
                                    -1  0  1 |]
 
 gradientY :: (Source a Float) => Array a DIM2 Float -> Array PC5 DIM2 Float
-gradientY = mapStencil2 (BoundConst 0) stencil
+gradientY = mapStencil2 BoundClamp stencil
         where stencil = [stencil2| 1  2  1
                                    0  0  0
                                   -1 -2 -1 |] 
-
 
 sobel :: (Source a Float) => Array a DIM2 Float -> (Array D DIM2 Float, Array D DIM2 Float)
 sobel img = (magnitudes, thetas)
@@ -31,3 +30,19 @@ sobel img = (magnitudes, thetas)
         thetaFromDiff x y = if x == 0 then if y < 0 then -pi else if y > 0 then pi else 0 else atan (y / x)
         magnitudes = R.zipWith (\x y -> sqrt ((x * x + y * y) / 9)) gx gy
         thetas = R.zipWith thetaFromDiff gx gy
+        
+gaussian :: (Source a Float) => Int -> Float -> Array a DIM2 Float -> Array D DIM2 Float
+gaussian width sigma = delay . mapStencil2 BoundClamp (generateGausKernel width sigma)
+        
+
+generateGausKernel :: Int -> Float -> Stencil DIM2 Float
+generateGausKernel width sigma = makeStencil2 width width genStencil
+        where
+        center = (truncate.(/ (2::Float)).fromIntegral) width
+        gaussianKernel = [exp(-( fromIntegral (x*x + y*y)/(2*sigma*sigma) )) | x <- [-center..center], y <- [-center..center]]
+        normalisedGaussian = R.fromListUnboxed (Z :. width :. width) (Prelude.map (/ sum gaussianKernel) gaussianKernel)
+        genStencil (Z:.x:.y) 
+                | x >= -center && x <= center && y >= -center && y <= center = Just (normalisedGaussian!(Z:.x+center:.y+center))
+                | otherwise = Nothing
+         
+

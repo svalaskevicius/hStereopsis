23bb625a8110de117a76d07941d40e0833c31aad
Sarunas Valaskevicius <rakatan@gmail.com>
2013-06-02 04:28:39 +0100

    use cabal-dev and switch to ps2pdf

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cabal configure --enable-executable-profiling --ghc-option=-auto-all
-cabal build
-time ./dist/build/hStereopsis/hStereopsis data/TeaPotStereoData/image0001_left.png +RTS -N4 -p -K100M -hc -sresult.summary
-cat hStereopsis.hp | hp2ps -c -s | pstopdf -i -o hStereopsis.pdf
+cabal-dev configure --enable-executable-profiling --ghc-option=-auto-all
+cabal-dev build
+time ./dist/build/hStereopsis/hStereopsis data/TeaPotStereoData/image0001_left.png data/TeaPotStereoData/image0001_right.png +RTS -N4 -p -K100M -hc -sresult.summary
+cat hStereopsis.hp | hp2ps -c -s | ps2pdf - hStereopsis.pdf
